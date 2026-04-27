6361b38407b1bd6bbd781f50ff9390e5b2ffd1ca
lsy3709 <lsy3709@naver.com>
2026-04-27 14:36:34 +0900

    5교시, 이벤트 핸들러 예시2, input 2개 확인

--- hello-react-vite2/src/ch04/App.jsx
+++ hello-react-vite2/src/ch04/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import EventPractice from './EventPractice';
+import EventPractice2 from './EventPractice2';
 
 // 부모 컴포넌트
 const App = () => {
@@ -7,7 +8,10 @@ const App = () => {
     <div>
       <h1>ch4-이벤트 핸들링</h1>
       {/* 자식 컴포넌트 : EventPractice , 그려주기 */}
+      <h2>연습1</h2>
       <EventPractice></EventPractice>
+      <h2>연습2</h2>
+      <EventPractice2></EventPractice2>
     </div>
   );
 };
--- hello-react-vite2/src/ch04/EventPractice.jsx
+++ hello-react-vite2/src/ch04/EventPractice.jsx
@@ -4,6 +4,16 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
   state = { message: '' };
 
+  // 이벤트 리스너,
+  handleChange = (e) => {
+    this.setState({ message: e.target.value });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({ message: '' });
+  };
+
   render() {
     return (
       <div>
@@ -14,18 +24,9 @@ class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({ message: e.target.value });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({ message: '' });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
